4d142d0542897627dc480a9fd97c9fb4611ee461
Andrej03 <andrej.bachvarovski@gmail.com>
2025-03-26 20:49:26 -0400

    core data model fix

--- LAB_ASSIGNMENT_2/Preview Content/Views/ContentView.swift
+++ LAB_ASSIGNMENT_2/Preview Content/Views/ContentView.swift
@@ -1,26 +1,26 @@
-import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        TabView {
-            NavigationView {
-                ProductDetailView()
-            }
-            .tabItem {
-                Label("Detail", systemImage: "doc.text.magnifyingglass")
-            }
-
-            ProductListView()
-                .tabItem {
-                    Label("List", systemImage: "list.bullet")
-                }
-
-            NavigationView {
-                AddProductView()
-            }
-            .tabItem {
-                Label("Add", systemImage: "plus.circle")
-            }
-        }
-    }
-}
+//import SwiftUI
+//
+//struct ContentView: View {
+//    var body: some View {
+//        TabView {
+//            NavigationView {
+//                ProductDetailView()
+//            }
+//            .tabItem {
+//                Label("Detail", systemImage: "doc.text.magnifyingglass")
+//            }
+//
+//            ProductListView()
+//                .tabItem {
+//                    Label("List", systemImage: "list.bullet")
+//                }
+//
+//            NavigationView {
+//                AddProductView()
+//            }
+//            .tabItem {
+//                Label("Add", systemImage: "plus.circle")
+//            }
+//        }
+//    }
+//}
