d3d9efb0bfe983c5f1cdbd3f8f1f33679a4204f1
Andrej03 <andrej.bachvarovski@gmail.com>
2025-03-28 14:42:27 -0400

    Persistance base set

--- LAB_ASSIGNMENT_2/Persistence.swift
+++ LAB_ASSIGNMENT_2/Persistence.swift
@@ -1,1 +1,22 @@
-	
+import Foundation
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+    let container: NSPersistentContainer
+    
+    private init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "Model")
+        if inMemory {
+            container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.loadPersistentStores(completionHandler: { _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        )
+        container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+}
+
